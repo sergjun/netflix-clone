7a73113bb6dc7c69cf76e437c2b4f0c0d561f1f7
sergjun <serrgio.madureira@gmail.com>
2021-07-22 21:15:48 -0300

    Feito o header

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import "./styles.css"
+
+function Header () {
+
+    return (
+        <header>
+            <p>Series</p>
+            <p>Filmes</p>
+            <p>Minha lista</p>
+        </header>
+        )
+
+}
+
+
+export { Header }

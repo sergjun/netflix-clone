5ab617bc9590b526443bb40d67efba66ea0e279a
joaoprferreira <joaof082@gmail.com>
2021-07-23 21:34:53 -0300

    fazendo requisição api
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
+import Main from '../../components/Main/Main'
 
 export default function Home() {
+  
+  const [ film, setFilm ] = useState("")
+
+  useEffect(() => {
+    const getFilmFromApi = async () => {
+      const data = await fetch("https://api.tvmaze.com/shows/1")
+      const response = await data.json()
+      console.log(response)
+      setFilm(response)
+    }
+    getFilmFromApi()
+  }, [])
+
+  
+
   return (
     <div> 
       <Header />
-      <h2>Iniciando a tela home</h2>
+      <Main 
+        film={film}
+      />
     </div>
   )
 }

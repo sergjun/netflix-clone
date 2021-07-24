ad31608c20510d6fffee64ae6d742baf4ecebc4a
sergjun <serrgio.madureira@gmail.com>
2021-07-24 00:14:36 -0300

    Criado uma funcao que randomiza o filme mostrado na pagina inicial

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,15 +2,25 @@ import React, { useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
 import Main from '../../components/Main/Main'
 
+function RandomFilm (max , min) {
+
+  
+  // console.log(Math.round(Math.random() * (max - min) + min))
+  return Math.round(Math.random() * (max - min) + min)
+ 
+}
+
+
+
 export default function Home() {
   
   const [ film, setFilm ] = useState("")
 
   useEffect(() => {
     const getFilmFromApi = async () => {
-      const data = await fetch("https://api.tvmaze.com/shows/1")
+      const data = await fetch(`https:/api.tvmaze.com/shows/${RandomFilm(1, 239)}`)
       const response = await data.json()
-      console.log(response)
+      // console.log(response)
       setFilm(response)
     }
     getFilmFromApi()

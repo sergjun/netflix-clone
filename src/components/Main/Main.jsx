1dd706629084cc05433afd10fd70dea4ab996bfd
sergjun <serrgio.madureira@gmail.com>
2021-07-24 00:13:12 -0300

    criada divs com os respectivos icones e textos, criado um .map para listar generos

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import "./MainStyle.scss"
+import playIcon from "../../assets/icons/play-button.png"
+import info from "../../assets/icons/info.png"
+import mais from "../../assets/icons/plus.png"
 
 
 export default function Main({ film }) {
 
-  const { image } = film
+  const { image , genres } = film
 
+  console.log (genres) 
   return (
     <div className="Main-container">
-      <img src={image && image.original} alt="" className="image-fundo" />
-
+      <div className="image-container">
+        <img src={image?.original} alt="" className="image-fundo" />
+      </div>
+      <ul className="genre">{genres?.map(genre =>  <li key={genre} >{genre}</li>)}</ul>
       <div className="">
 
         <section className="button-towatch">
-          <p>Assistir</p>
+          <div className="plus">
+            <img src={mais} alt ="plus icon"></img>
+              <p>Minha lista</p>
+          </div>
+          <div className="play">
+            <img src={playIcon} alt = "play icon"></img>
+              <p>Assistir</p>
+          </div>
+          <div className="info">
+            <img src={info} alt = "info icon"></img>
+              <p>Saiba mais</p>
+          </div>
           <img src="" alt="" />
         </section>
         

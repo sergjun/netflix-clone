7d737db2b9580c8e3c28d1b5d339345c2f0fb6ce
sergjun <serrgio.madureira@gmail.com>
2021-07-24 23:38:39 -0300

    Feito o fetch da lista de previas e renderizada as mesmas

--- src/components/Previas/Previas.jsx
+++ src/components/Previas/Previas.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./PreviasStyle.scss"
+
+
+
 
 export default function Previas() {
+  
+  const [listFilm, setList] = useState([])
+  useEffect(() => {
+    const getListFromApi = async () => {
+      const data = await fetch(`https:/api.tvmaze.com/shows`)
+      const response = await data.json()
+       console.log(response)
+      setList(response)
+    }
+    getListFromApi()
+  }, [])
+    
+  const listamenor = listFilm.slice(0,10)
+
+  console.log(listFilm)
+
   return (
     <div>
-      <h1>Component de previas</h1>
+      <h3>Prévias</h3>
+       <div className="lista-container">
+         
+           {listamenor?.map(function (list) {
+            return <img className="lista-imagem" src={list.image.original} alt ={list.name} key = {list.id}></img> 
+           
+          })}
+         
+       </div>
+
        </div>
   )
 }

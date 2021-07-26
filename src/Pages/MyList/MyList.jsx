9d7394d0a6594228b6aeef058936b4acd4486ce8
joaoprferreira <joaof082@gmail.com>
2021-07-26 19:08:30 -0300

    iniciando a pagina de lista de filmes

---
+++ src/Pages/MyList/MyList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './MyListStyle.scss'
+
+export function MyList() {
+  return (
+    <div className="my-list-page-container">
+      <h2 style={{ color: "white" }}>My Lsit </h2>
+    </div>
+  )
+}

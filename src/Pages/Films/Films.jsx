1c6cfda05d066f85d1214657f8143c90a4f92355
joaoprferreira <joaof082@gmail.com>
2021-07-26 19:09:07 -0300

    Iniciando pagina de filmes

---
+++ src/Pages/Films/Films.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./FilmsStyle.scss"
+
+export function Films() {
+  return (
+    <div className="films-container">
+      <h2 style={{ color: "white" }}>Films page</h2>;
+    </div>
+  );
+}

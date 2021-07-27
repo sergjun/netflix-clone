b71f3e98ef5f013c1600c890818b4433ae77ceb9
joaoprferreira <joaof082@gmail.com>
2021-07-27 18:06:14 -0300

    implementando classes
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/Pages/MyList/MyList.jsx
+++ src/Pages/MyList/MyList.jsx
@@ -7,25 +7,27 @@ import search from "../../assets/icons/search.svg"
 export function MyList() {
   return (
     <>
-    <header>
-      <img src={back} alt="" />
+      <header className="header-component">
+        <img src={back} alt="" className="header__icon--back" />
 
-      <h3>Minhas listas</h3>
-    </header>
-    <div className="list-container">
-      <input type="text" placeholder="Procure entre listas" />
-      <img className ="search" src={search} alt="" />
-          <section className="main-list">
-            <div className="Add-Film">
-              <p className="plus-icon">+</p>
+        <h3 className="header__text--title">Minhas listas</h3>
+      </header>
 
-            </div>
-               <p className ="list-item">
-                Criar lista
-               </p>
+      <div className="list-container">
 
-          </section>
-    </div>
+        <input type="text" placeholder="Procure entre listas" className="header__search--input" />
+
+        <img className="header__search--icon" src={search} alt="" />
+
+        <section className="main-list">
+
+          <div className="Add-Film">
+            <p className="plus-icon">+</p>
+          </div>
+
+          <p className="list-item"> Criar lista </p>
+        </section>
+      </div>
     </>
   )
 }

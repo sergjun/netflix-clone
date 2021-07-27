58ad1fbae7a01c044fe99bad080d339e7fb8a040
joaoprferreira <joaof082@gmail.com>
2021-07-26 22:36:16 -0300

    Criando o component "continuar assistindo"
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

---
+++ src/components/keepWatching/KeepWatching.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./KeepWatchingStyle.scss"
+
+
+export function KeepWatching() {
+  return (
+    <div className="keep-watching-container">
+      <h1 className="title">Continuar assistindo como PEDRO</h1>
+    </div>
+  )
+}

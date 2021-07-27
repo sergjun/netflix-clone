79bbd0119b28bb79d64ecfb5f5336971dc3f64d2
sergjun <serrgio.madureira@gmail.com>
2021-07-26 22:42:20 -0300

    Estilizacao da pagina Minha Lista e criação basica da mesma

--- src/Pages/MyList/MyList.jsx
+++ src/Pages/MyList/MyList.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import './MyListStyle.scss'
+import back from "../../assets/img/proximo.png"
+import search from "../../assets/icons/search.svg"
+
 
 export function MyList() {
   return (
-    <div className="my-list-page-container">
-      <h2 style={{ color: "white" }}>My Lsit </h2>
+    <>
+    <header>
+      <img src={back} alt="" />
+
+      <h3>Minhas listas</h3>
+    </header>
+    <div className="list-container">
+      <input type="text" placeholder="Procure entre listas" />
+      <img className ="search" src={search} alt="" />
+          <section className="main-list">
+            <div className="Add-Film">
+              <p className="plus-icon">+</p>
+
+            </div>
+               <p className ="list-item">
+                Criar lista
+               </p>
+
+          </section>
     </div>
+    </>
   )
 }
